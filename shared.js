6180f5eb45ca025954fb1e9e9e3ddf01704c8679
evgenia <evgenia@resdiary.com>
2020-06-13 11:15:58 +0100

    Complete the CSS Grid

--- shared.js
+++ shared.js
@@ -4,14 +4,18 @@ var selectPlanButtons = document.querySelectorAll('.plan button');
 var negativeResponseButton = document.querySelector(".modal__action.modal__action--negative");
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta');
 
-console.dir(negativeResponseButton);
+// console.dir(negativeResponseButton);
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', () => {
         // modal.style.display = "block";
         // backdrop.style.display = 'block';
         modal.classList.add('open');
-        backdrop.classList.add('open');
+        backdrop.style.display = 'block';
+        setTimeout(function () {
+            backdrop.classList.add('open');
+        }, 10);
     });
 }
 
@@ -24,17 +28,34 @@ if (negativeResponseButton) {
     negativeResponseButton.addEventListener('click', closeModal);
 }
 
-
-toggleButton.addEventListener('click', function () {
-    //mobileNav.style.display = 'block';
-    //backdrop.style.display = 'block';
-    mobileNav.classList.add("open");
-    backdrop.classList.add('open');
-})
-
 function closeModal() {
     if (modal) {
         modal.classList.remove('open');
     }
     backdrop.classList.remove('open');
+    setTimeout(function () {
+        backdrop.style.display = 'none';
+    }, 10);
 }
+
+toggleButton.addEventListener('click', function () {
+    //mobileNav.style.display = 'block';
+    //backdrop.style.display = 'block';
+    mobileNav.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function () {
+        backdrop.classList.add('open');
+    }, 10);
+})
+
+ctaButton.addEventListener('animationstart', function (event) {
+    console.log('Animation started', event);
+});
+
+ctaButton.addEventListener('animationend', function (event) {
+    console.log('Animation finished', event);
+})
+
+ctaButton.addEventListener('animationiteration', function (event) {
+    console.log('Animation iteration', event);
+})
